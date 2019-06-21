8ff4c714b67754cd204cbd2a6d6e9074710a2c33
jroseman <jroseman@jroseman-m01.ad.savagebeast.com>
2019-06-21 12:57:56 -0400

    added a night job function that downloads spotify chart data

--- app.js
+++ app.js
@@ -7,22 +7,9 @@ app.get('/', (req, res) => {
     res.sendFile('public/index.html', { root : __dirname});
 });
 
-app.get('/download_spotify_top200/:where/:how/:when', (req, res) => {
-    data_layer.spotifyTop200(req.params.where, req.params.how, req.params.when);
-    var firstLetter = req.params.how.charAt(0).toUpperCase();
-    var firstLetter = req.params.how.slice(1);
-    res.send(req.params.how.charAt(0).toUpperCase() + req.params.how.slice(1) + 
-    ' top 200 Tracks on Spotify in ' + req.params.where.toUpperCase());
-});
-
-app.get('/download_spotify_viral50/:where/:how/:when', (req, res) => {
-    data_layer.spotifyViral50(req.params.where, req.params.how, req.params.when);
-    res.send(req.params.how.charAt(0).toUpperCase() + req.params.how.slice(1) 
-     + ' viral 50 Tracks on Spotify in ' + req.params.where.toUpperCase());
-});
-
-app.get('/badpage', (req, res) => {
-    res.send('Something went wrong!');
+app.get('/nightJob', (req, res) => {
+    data_layer.nightJob();
+    res.send("night job complete");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- data_layer.js
+++ data_layer.js
@@ -1,11 +1,13 @@
 const Fs = require('fs');
 const Path = require('path');
 const Axios = require('axios');
+const csv = require('csvtojson');
+
 var exports = {};
 
-exports.spotifyViral50 = async function(where, how, when) {
-  const url = 'https://spotifycharts.com/viral/' + where + '/' + how + '/' + when + '/download';
-  const path = Path.resolve(__dirname, 'files', where + '_' + how + '_' + when + '.csv');
+async function download(url) {
+  const dataPeriod = (url.charAt(38) === 'w' ? "weekly" : "daily");
+  const path = Path.resolve(__dirname, 'files', 'us_' + dataPeriod + '_latest.csv');
   const writer = Fs.createWriteStream(path);
   const response = await Axios({
     method: 'GET',
@@ -23,37 +25,12 @@ exports.spotifyViral50 = async function(where, how, when) {
     writer.on('error', err => {
       reject(err);
     });
-
-  }).then(() => {
-    console.log('Spotify Viral 50 successfully downloaded');
   });
 }
 
-exports.spotifyTop200 = async function(where, how, when) {
-
-  const url = 'https://spotifycharts.com/regional/' + where + '/' + how + '/' + when + '/download';
-  const path = Path.resolve(__dirname, 'files', where + '_' + how + '_' + when + '.csv');
-  const writer = Fs.createWriteStream(path);
-  const response = await Axios({
-    method: 'GET',
-    url,
-    responseType: 'stream'
-  });
-
-  response.data.pipe(writer);
-
-  return new Promise((resolve, reject) => {
-    writer.on('end', () => {
-      resolve();
-    });
-
-    writer.on('error', err => {
-      reject(err);
-    });
-
-  }).then(() => {
-    console.log('Spotify Top 200 successfully downloaded');
-  });
+exports.nightJob = function() {
+  download("https://spotifycharts.com/regional/us/daily/latest/download");
+  download("https://spotifycharts.com/regional/us/weekly/latest/download");
 }
 
 module.exports = exports;
